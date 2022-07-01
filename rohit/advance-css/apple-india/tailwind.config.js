1a5ebc68bbc6357180e0aea9512b712b0d10ce10
Rohit <rohitkumar252526@gmail.com>
2022-07-01 21:28:15 +0530

    designing apple-india homepage using tailwind css

--- rohit/advance-css/apple-india/tailwind.config.js
+++ rohit/advance-css/apple-india/tailwind.config.js
@@ -4,19 +4,5 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
-      'black' : '#000000'
-    },
-  },
+
 }
